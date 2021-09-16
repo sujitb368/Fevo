a6e5d77c99e9f321a81ca6209ead9438bbc05ff2
sujit <sujitb368@gmail.com>
2021-09-16 09:38:12 +0530

    2nd commit after some change

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// gsap.registerPlugin(ScrollTriger);
 
-// const img = document.querySelector("img")
 const to_animate = document.querySelectorAll(".to_animate");
-// console.log(to_animate);
+
 const callback_fun = (entries, observer)=>{
     entries.forEach( (entry) => {
         if(entry.isIntersecting){
             console.log("element : ",entry.target );
-
-            // entry.target.classList.add("slide_up");
-            // entry.target.style.animation = entry.target.dataset.animate;
-            // observer.unobserve(entry.target)
-
             gsap.from(".circle_product > *",{
                 y:30,
                 stagger: 0.2,
                 opacity:0
-
                
             })
         }      
